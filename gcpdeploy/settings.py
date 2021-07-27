29ba1c4d319b7fe03dbd4e3c37051fc56996af81
madhu86280 <madhubanga22@gmail.com>
2021-07-27 20:56:06 +0530

    settings new changes4

--- gcpdeploy/settings.py
+++ gcpdeploy/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c!%obg9r9^1uqeu-z19fqf#ok9!cwo)ltsc(un^1a1%t63#%ai'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://deployment-heroku1.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['deployment-heroku1.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
