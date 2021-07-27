cd89c389c28762880d4f3a07396fc66419821199
madhu86280 <madhubanga22@gmail.com>
2021-07-27 19:39:06 +0530

    deploy1

--- gcpdeploy/settings.py
+++ gcpdeploy/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +130,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#added code
+STATIC_ROOT = os.join(BASE_DIR,'staticfiles')
